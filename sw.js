227f9d49af6cff5ab20048bf2670893d1cbd1700
berat-arsln <beratarsln@gmail.com>
2026-03-17 09:44:01 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'kelime-oyunu-v1';
+// Buraya çevrimdışı olmasını istediğin tüm dosyaları yaz (örneğin resimler varsa onları da ekle)
+const assets = [
+  './',
+  './index.html',
+  './style.css', // Eğer CSS dışarıdaysa
+  './script.js'  // Eğer JS dışarıdaysa
+];
+
+// Dosyaları yükleme (Cache) - Güncellenmiş Halı
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // return eklemek tüm dosyaların indiğinden emin olmanı sağlar
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// İnternet yokken dosyaları "Kutu"dan getirme
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
